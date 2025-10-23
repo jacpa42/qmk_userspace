3b49fac88fadf7eed71ad0392beb6a83764932b8
jacpa42 <jacobenthoven@gmail.com>
2025-10-23 23:30:58 +0200

    changed one key
    Changed the top right key on the base layer to ampersand as i need that
    shit

--- keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
@@ -72,9 +72,9 @@ static uint16_t auto_pointer_layer_timer = 0;
 /** \brief QWERTY layout (3 rows, 10 columns). */
 // note(jacob): I changed p to be 1 layer down and to include semi-colon above p
 #define LAYOUT_LAYER_BASE                                                                          \
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O, KC_SEMICOLON, \
-       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,         KC_P, \
-       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,      KC_SLSH, \
+       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,  KC_AMPR, \
+       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_P, \
+       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, \
         LT_NAV(ESC), LT_NUM(SPC), LT_MED(TAB),              LT_FUN(ENT), LT_SYM(BSPC)
 
 /** Convenience row shorthands. */
@@ -154,7 +154,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 // ^ ~ - = b
 // note(jacob): Added ent and bspc keys for nicer time with numeral layer
 #define LAYOUT_LAYER_NUMERAL                                                                  \
-    _______, _______, KC_LABK, KC_RABK,  _______,  LSFT(KC_GRV), KC_1,  KC_2,  KC_3,  KC_EQL, \
+    _______, _______, KC_LABK, KC_RABK,  _______,  KC_AMPR, KC_1,  KC_2,  KC_3,  KC_EQL, \
     KC_SCLN, KC_PERC, KC_PLUS, KC_ASTR,  _______,  KC_PIPE,      KC_4,  KC_5,  KC_6,  KC_0, \
     KC_CIRC, KC_TILD, KC_MINS,  KC_EQL,  _______,   KC_GRV,      KC_7,  KC_8,  KC_9,  KC_CIRC, \
                        XXXXXXX, _______, XXXXXXX,   KC_ENT, KC_BSPC
