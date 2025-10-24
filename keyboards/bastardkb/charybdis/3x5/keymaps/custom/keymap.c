bc6285236b99bbed165e5087d28b8fec79273783
jacpa42 <jacobenthoven@gmail.com>
2025-10-24 09:20:41 +0200

    Removed the function layer
    The space key was quite annoying because i use it as leader in vim so
    the permissive tap was fucking shit up. Plain space bar now no layer
    there and removed the function layer (also it is useless)

--- keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
@@ -22,7 +22,6 @@
 
 enum charybdis_keymap_layers {
   LAYER_BASE = 0,
-  LAYER_FUNCTION,
   LAYER_NAVIGATION,
   LAYER_MEDIA,
   LAYER_POINTER,
@@ -45,7 +44,6 @@ static uint16_t auto_pointer_layer_timer = 0;
 #endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
 #endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
-#define LT_FUN(key) LT(LAYER_FUNCTION, KC_##key)
 #define LT_NAV(key) LT(LAYER_NAVIGATION, KC_##key)
 #define LT_MED(key) LT(LAYER_MEDIA, KC_##key)
 #define LT_NUM(key) LT(LAYER_NUMERAL, KC_##key)
@@ -66,7 +64,7 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,  KC_AMPR, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_P, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, \
-        LT_NAV(ESC), LT_NUM(SPC), LT_MED(TAB),              LT_FUN(ENT), LT_SYM(BSPC)
+        LT_NAV(ESC), KC_SPC, LT_NUM(TAB),              LT_MED(ENT), LT_SYM(BSPC)
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -84,20 +82,6 @@ static uint16_t auto_pointer_layer_timer = 0;
  */
 
 /**
- * \brief Function layer.
- *
- * Secondary right-hand layer has function keys mirroring the numerals on the
- * primary layer with extras on the pinkie column, plus system keys on the inner
- * column. App is on the tertiary thumb key and other thumb keys are duplicated
- * from the base layer to enable auto-repeat.
- */
-#define LAYOUT_LAYER_FUNCTION                                                                 \
-    _______________DEAD_HALF_ROW_______________, KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, \
-    ______________HOME_ROW_GACS_L______________, KC_SCRL,   KC_F4,   KC_F5,   KC_F6,  KC_F11, \
-    _______________DEAD_HALF_ROW_______________, KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10, \
-                      XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX
-
-/**
  * \brief Media layer.
  *
  * Tertiary left- and right-hand layer is media and RGB control.  This layer is
@@ -108,7 +92,7 @@ static uint16_t auto_pointer_layer_timer = 0;
     _______________DEAD_HALF_ROW_______________, _______________DEAD_HALF_ROW_______________, \
     XXXXXXX, KC_MUTE, KC_VOLD, KC_MPRV, XXXXXXX, XXXXXXX, KC_MNXT, KC_VOLU, KC_MUTE, XXXXXXX, \
     XXXXXXX, XXXXXXX, XXXXXXX,  EE_CLR, QK_BOOT, QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, \
-                      XXXXXXX, XXXXXXX, _______, KC_MSTP, KC_MPLY
+                      XXXXXXX, KC_MPLY, , _______, XXXXXXX
 
 /** \brief Mouse emulation and pointer functions. */
 #define LAYOUT_LAYER_POINTER                                                                  \
@@ -148,7 +132,7 @@ static uint16_t auto_pointer_layer_timer = 0;
     _______, KC_AMPR, KC_LABK, KC_RABK,  _______, KC_AMPR, KC_1,  KC_2,  KC_3,  KC_EQL, \
     KC_SCLN, KC_PERC, KC_PLUS, KC_ASTR,  _______, KC_PIPE, KC_4,  KC_5,  KC_6,  KC_0, \
     KC_CIRC, KC_TILD, KC_MINS,  KC_EQL,  _______,  KC_GRV, KC_7,  KC_8,  KC_9,  KC_CIRC, \
-                       XXXXXXX, _______, XXXXXXX,  KC_ENT, KC_BSPC
+                       XXXXXXX, XXXXXXX, _______,  KC_ENT, KC_BSPC
 
 /**
  * \brief Symbols layer.
@@ -162,7 +146,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 // ; [ ( { g   h @ _ : $
 // ` ] ) } b   n m , . #
 #define LAYOUT_LAYER_SYMBOLS                                                                  \
-    _______,  KC_BSLS, KC_QUOT, KC_DQUO, _______, _______________DEAD_HALF_ROW_______________, \
+    _______,  KC_BSLS, KC_QUOT, KC_DQUO, _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, \
     KC_SCLN,  KC_LBRC, KC_LPRN, KC_LCBR, KC_LABK, _______,   KC_AT, KC_UNDS, KC_COLN,  KC_DLR, \
      KC_GRV,  KC_RBRC, KC_RPRN, KC_RCBR, KC_RABK, _______, _______, KC_COMM, KC_DOT, KC_HASH, \
                       KC_EXLM, KC_PERC, KC_UNDS,  XXXXXXX, _______
@@ -218,7 +202,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_wrapper(
     POINTER_MOD(HOME_ROW_MOD_GACS(LAYOUT_LAYER_BASE))
   ),
-  [LAYER_FUNCTION] = LAYOUT_wrapper(LAYOUT_LAYER_FUNCTION),
   [LAYER_NAVIGATION] = LAYOUT_wrapper(LAYOUT_LAYER_NAVIGATION),
   [LAYER_MEDIA] = LAYOUT_wrapper(LAYOUT_LAYER_MEDIA),
   [LAYER_NUMERAL] = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
