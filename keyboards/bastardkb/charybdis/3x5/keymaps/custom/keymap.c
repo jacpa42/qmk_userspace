a33f3d0dd2c09b6d9b3b651bdc068e3a3f7b9bb1
jacpa42 <jacobenthoven@gmail.com>
2025-11-28 09:13:35 +0200

    fixed typo

--- keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
@@ -64,7 +64,7 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,  KC_EXLM, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_P, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, \
-        KC_ESC,  LT_NUM(KC_SPC), LT_NAV(TAB),             LT_MED(ENT), LT_SYM(BSPC)
+        KC_ESC,  LT_NUM(SPC), LT_NAV(TAB),             LT_MED(ENT), LT_SYM(BSPC)
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
