132857fa5ccbebd597ab0771e32a60515bd0d587
jacpa42 <jacobenthoven@gmail.com>
2025-11-28 07:33:46 +0200

    swapped tab and space

--- keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
@@ -66,7 +66,7 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,  KC_AMPR, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_P, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, \
-        LT_NAV(ESC), LT_NUM(SPC), LT_FUN(TAB),              LT_MED(ENT), LT_SYM(BSPC)
+        LT_NAV(ESC), LT_NUM(TAB), LT_FUN(SPC),              LT_MED(ENT), LT_SYM(BSPC)
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
